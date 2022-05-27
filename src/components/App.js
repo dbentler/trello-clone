9b2d7c141b4f645d5c780d468b41e73a95e3e11c
dbentler <db3857@nyu.edu>
2022-05-26 22:07:19 -0400

    TrelloList component boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from "react";
+import TrelloList from "./TrelloList";
 
 function App() {
   return (
     <div className="App">
-     <h2>
-       Hello World
-     </h2>
+     <h2>Hello World</h2>
+     <TrelloList title="test" />
     </div>
   );
 }
